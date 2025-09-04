871a94dde1c9ddf672eaf3a4180a6c37d806b53e
artur tomaz <arturtomaz1@gmail.com>
2025-09-04 19:36:13 -0300

    Hero image adjust

--- portifolio/src/sections/HeroArea.jsx
+++ portifolio/src/sections/HeroArea.jsx
@@ -3,7 +3,10 @@ import TecnologiaHero from "../components/TecnologiaHero";
 
 function HeroArea() {
   return (
-    <section id="home" className="h-fit flex flex-col lg:flex-row items-center justify-between pb-[80px] sm:pb-[140px] 2xl:px-10">
+    <section
+      id="home"
+      className="h-fit flex flex-col lg:flex-row items-center justify-between pb-[80px] sm:pb-[140px] 2xl:px-10"
+    >
       {/* ESQUERDA */}
       <div className="flex-1 flex flex-col h-[100%] gap-5">
         {/* Hero Title */}
@@ -30,23 +33,22 @@ function HeroArea() {
         </div>
         {/* Tecnologias Principais */}
         <div className="flex flex-wrap gap-y-3 gap-x-4 w-[95%]">
-          <TecnologiaHero nome="React"/>
-          <TecnologiaHero nome="Javascript"/>
-          <TecnologiaHero nome="Node.js"/>
-          <TecnologiaHero nome="PostgreSQL"/>
+          <TecnologiaHero nome="React" />
+          <TecnologiaHero nome="Javascript" />
+          <TecnologiaHero nome="Node.js" />
+          <TecnologiaHero nome="PostgreSQL" />
         </div>
         {/* Botões */}
         <div className="flex gap-4 sm:gap-8 mt-5">
           <BotaoHero nome="Projetos" link="projects" icon="newtab" />
-          <BotaoHero nome="Contato" link="contact" icon="mail"/>
+          <BotaoHero nome="Contato" link="contact" icon="mail" />
         </div>
       </div>
-
 
       {/* DIREITA */}
       <div className="z-2 flex-1 h-[100%] mt-[50px]">
         <div className="flex justify-center items-center">
-          <img className="sm:max-w-[550px]" src="/Main Hero Teste.png" alt="" />
+          <img className="sm:max-w-[550px]" src="/Main Hero.png" alt="" />
         </div>
       </div>
     </section>
